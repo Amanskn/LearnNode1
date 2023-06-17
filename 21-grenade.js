0a35328f2c72c471f2b46192aeab5706cb34dd32
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-17 09:34:51 +0530

    Done with destructuring stuff on a small scale

---
+++ 21-grenade.js
@@ -0,0 +1,9 @@
+var obj = {
+    num1:12,
+    num2:32
+}
+const {num1,num2}=obj
+console.log(obj);
+var newObj={num1:54,num2,num1,num2:89};
+console.log("This is the new obj",newObj)
+console.log(num1,num2)
